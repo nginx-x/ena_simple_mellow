f6dafc0a8461e1bc74662f5faae41f762ae7d8fb
김근홍 <geunhong.kim@ghkim.net>
2022-12-19 00:44:15 +0000

    first commit

---
+++ js.default.js
@@ -0,0 +1,11 @@
+// <![CDATA[
+jQuery(function($){
+  // delete the border for the last row
+	// $('.board_list tr:last-child>td').css('border','0');
+	// hide last tag
+	$('.read_footer .tags span:last-child').hide();
+	// delete the margin-top for the first child of the ccomments
+	$('.feedback .xe_content>*:first-child').css('margin-top','0');
+	
+});
+// ]]>
